678d8c178ffa5de6a5eddcb712391b2db9f097c5
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2024-11-22 11:03:47 +0100

    color shades in constants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,82 @@
+const primaryShades = {
+	50: '#fceae7',
+	100: '#f8d0c8',
+	200: '#f1a899',
+	300: '#e97f6a',
+	400: '#e2563b',
+	500: '#d95d44', // Base color
+	600: '#c2543d',
+	700: '#a94a34',
+	800: '#8f3f2b',
+	900: '#753422',
+	DEFAULT: '#d95d44', // Base color
+};
+const secondaryShades = {
+	50: '#f5f6fb',
+	100: '#e6e8f5',
+	200: '#cdd1eb',
+	300: '#b3b9e0',
+	400: '#9aa2d6',
+	500: '#8388B6', // Base color
+	600: '#6b6f9e',
+	700: '#545686',
+	800: '#3c3e6e',
+	900: '#242656',
+	DEFAULT: '#8388B6', // Base color
+};
+const tertiaryShades = {
+	50: '#f9eef9',
+	100: '#f2d6f2',
+	200: '#e5b0e5',
+	300: '#d88ad8',
+	400: '#cb64cb',
+	500: '#C089BB', // Base color
+	600: '#a05fa0',
+	700: '#804780',
+	800: '#603060',
+	900: '#401940',
+	DEFAULT: '#C089BB', // Base color
+};
+const neutralShades = {
+	50: '#f9f9f9',
+	100: '#f2f2f2',
+	200: '#e6e6e6',
+	300: '#d9d9d9',
+	400: '#cccccc',
+	500: '#cfcfcf', // Base color
+	600: '#b3b3b3',
+	700: '#999999',
+	800: '#7f7f7f',
+	900: '#666666',
+	DEFAULT: '#cfcfcf', // Base color
+};
+const successShades = {
+	50: '#e6f4ec',
+	100: '#cce9d9',
+	200: '#99d3b3',
+	300: '#66bd8d',
+	400: '#33a767',
+	500: '#75B996', // Base color
+	600: '#2a8a3b',
+	700: '#20702e',
+	800: '#155621',
+	900: '#0b3c14',
+	DEFAULT: '#75B996', // Base color
+};
+const warningShades = {
+	50: '#fdf4e6',
+	100: '#fbe9cc',
+	200: '#f7d399',
+	300: '#f3bd66',
+	400: '#efa733',
+	500: '#E3BA58', // Base color
+	600: '#e58a00',
+	700: '#b36c00',
+	800: '#804d00',
+	900: '#4d2e00',
+	DEFAULT: '#E3BA58', // Base color
+};
+
 module.exports = {
 	content: ['./**/*.php'],
 	theme: {
@@ -40,170 +119,77 @@ module.exports = {
 			defaultTheme: {
 				extend: {
 					colors: {
-						primary: {
-							50: '#e5eeff',
-							100: '#cfe0ff',
-							200: '#a9c3ff',
-							300: '#7599ff',
-							400: '#3f5dff',
-							500: '#1423ff',
-							600: '#0008ff',
-							700: '#0009ff',
-							800: '#0008e3',
-							900: '#000094',
-							DEFAULT: '#000068',
-						},
-						secondary: {
-							50: '#f7f7f7',
-							DEFAULT: '#f7f7f7',
-							100: '#e3e3e3',
-							200: '#c8c8c8',
-							300: '#a4a4a4',
-							400: '#818181',
-							500: '#666666',
-							600: '#515151',
-							700: '#434343',
-							800: '#383838',
-							900: '#000000',
-						},
-						focus: {
-							50: '#e7f6ff',
-							100: '#d3efff',
-							200: '#b0dfff',
-							300: '#81c8ff',
-							400: '#4fa1ff',
-							500: '#2878ff',
-							DEFAULT: '#2878ff',
-							600: '#044bff',
-							700: '#0049ff',
-							800: '#0038c4',
-							900: '#0b3aa4',
-						},
 						black: {
 							DEFAULT: '#212121',
 						},
 						white: {
 							DEFAULT: '#F7F7F7',
 						},
-						error: {
-							50: '#fff0f2',
-							100: '#ffe2e6',
-							200: '#ffc9d4',
-							300: '#ff9db1',
-							400: '#ff6688',
-							DEFAULT: '#ff3164',
-							600: '#f21b5a',
-							650: '#f21b5a',
-							700: '#cb0544',
-							800: '#B0003E',
-							900: '#910a3d',
-						},
-						neutral: {
-							50: '#f7f7f7',
-							100: '#DCDCDC',
-							200: '#c8c8c8',
-							300: '#a4a4a4',
-							400: '#797979',
-							DEFAULT: '#F6F6F6',
-							500: '#666666',
-							600: '#515151',
-							700: '#434343',
-							800: '#383838',
-							900: '#313131',
-						},
-						success: {
-							50: '#edfcf4',
-							100: '#d2f9e2',
-							200: '#a9f1cb',
-							300: '#72e3ae',
-							400: '#39ce8e',
-							500: '#15b474',
-							600: '#0a915e',
-							DEFAULT: '#087951',
-							800: '#095c3f',
-							900: '#084c35',
-						},
-						accent: {
-							50: '#fcffe5',
-							100: '#f5ffc8',
-							DEFAULT: '#ebff9a',
-							300: '#d8fb5b',
-							400: '#c4f229',
-							500: '#a5d80a',
-							600: '#80ad03',
-							700: '#608308',
-							800: '#4d670d',
-							900: '#415710',
-						},
-						warning: {
-							50: '#fffaec',
-							100: '#fff3d3',
-							200: '#ffe4a5',
-							300: '#ffce6d',
-							400: '#ffae32',
-							500: '#ff930a',
-							DEFAULT: '#ff7b00',
-							700: '#cc5902',
-							800: '#a1450b',
-							900: '#823b0c',
-							950: '#461b04',
-						},
+						primary: primaryShades,
+						error: primaryShades,
+						accent: primaryShades,
+						focus: primaryShades,
+						secondary: secondaryShades,
+						tertiary: tertiaryShades,
+						neutral: neutralShades,
+						success: successShades,
+						warning: warningShades,
 					},
 				},
 			},
-			themes: [
-				{
-					name: 'primary',
-					extend: {
-						colors: {
-							palerose: {
-								DEFAULT: '#FBC5B4',
-							},
-							palepurple: {
-								DEFAULT: '#E0D3FF',
-							},
-							palepeach: {
-								DEFAULT: '#FADAB6',
-							},
-							palecyan: {
-								DEFAULT: '#D4E9DE',
-							},
-							palepink: {
-								DEFAULT: '#F7DAEF',
-							},
-							paleblue: {
-								DEFAULT: '#C0D8F4',
-							},
-							palegreen: {
-								DEFAULT: '#E3F9D6',
-							},
-							default: {
-								DEFAULT: '#d5d2d7',
-							},
-						},
-					},
-				},
-				{
-					name: 'secondary',
-					extend: {
-						colors: {
-							red: {
-								DEFAULT: '#D04425',
-							},
-							cyan: {
-								DEFAULT: '#00B9FF',
-							},
-							blue: {
-								DEFAULT: '#3A25FF',
-								200: '#EEEAFF',
-							},
-							black: {
-								DEFAULT: '#000',
-							},
-						},
-					},
-				},
-			],
+			// themes: [
+			// 	{
+			// 		name: 'primary',
+			// 		extend: {
+			// 			colors: {
+			// 				palerose: {
+			// 					DEFAULT: '#FBC5B4',
+			// 				},
+			// 				palepurple: {
+			// 					DEFAULT: '#E0D3FF',
+			// 				},
+			// 				palepeach: {
+			// 					DEFAULT: '#FADAB6',
+			// 				},
+			// 				palecyan: {
+			// 					DEFAULT: '#D4E9DE',
+			// 				},
+			// 				palepink: {
+			// 					DEFAULT: '#F7DAEF',
+			// 				},
+			// 				paleblue: {
+			// 					DEFAULT: '#C0D8F4',
+			// 				},
+			// 				palegreen: {
+			// 					DEFAULT: '#E3F9D6',
+			// 				},
+			// 				default: {
+			// 					DEFAULT: '#d5d2d7',
+			// 				},
+			// 			},
+			// 		},
+			// 	},
+			// 	{
+			// 		name: 'secondary',
+			// 		extend: {
+			// 			colors: {
+			// 				red: {
+			// 					DEFAULT: '#D04425',
+			// 				},
+			// 				cyan: {
+			// 					DEFAULT: '#00B9FF',
+			// 				},
+			// 				blue: {
+			// 					DEFAULT: '#3A25FF',
+			// 					200: '#EEEAFF',
+			// 				},
+			// 				black: {
+			// 					DEFAULT: '#000',
+			// 				},
+			// 			},
+			// 		},
+			// 	},
+			// ],
 		}),
 	],
 };
